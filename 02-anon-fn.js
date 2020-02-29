9d48482639c93f33cdcd20f5b53acfba49a8406e
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-02-29 00:08:02 -0800

    forEach anon w arg

---
+++ 02-anon-fn.js
@@ -0,0 +1,16 @@
+// array helper method: forEach()
+// with anonymous function and argument
+
+const days = ['M', 'T', 'W', 'R', 'F'];
+
+// prints out array content
+days.forEach(function(blahBlahBlah) {
+    console.log(blahBlahBlah);
+});
+
+console.log('convention:');
+
+// convention would be to use 'day' instead
+days.forEach(function(day) {
+    console.log(day);
+});
