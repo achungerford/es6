271475c24930b86a370448f0804867501b628174
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-01 13:26:43 -0800

    forEach element & index

--- 02-anon-fn.js
+++ 02-anon-fn.js
@@ -14,3 +14,18 @@ console.log('\nconvention:');
 days.forEach(function(day) {
     console.log(day);
 });
+
+console.log('\n');
+
+
+// if you want the index, need a second argument
+days.forEach(function(day, x) {
+    console.log(x);    // print the index only
+})
+
+console.log('\n');
+
+// if you want the index, need a second argument
+days.forEach(function(day, x) {
+    console.log(day, x);    // print the elements and their index
+})
