0349153ac0dd50fb33861710622f728b6ca30e4f
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-02 00:47:39 -0800

    filter data

--- 08-filter.js
+++ 08-filter.js
@@ -8,11 +8,16 @@ from a particular author.
 var products = [
     {name: 'cucumber', type: 'vegetable', qty: 0, price: 1},
     {name: 'banana', type: 'fruit', qty: 10, price: 15},
-    {name: 'celery', type: 'vegetable', qty: 30, price: 13},
+    {name: 'celery', type: 'vegetable', qty: 30, price: 6},
     {name: 'orange', type: 'fruit', qty: 3, price: 5}
 ];
 
 // get subset: vegetables, qty > 0, price < 10
 
+result = products.filter(function(product) {
+    return product.type === 'vegetable'
+        && product.qty > 0
+        && product.price < 10;
+});
 
-
+console.log(result);
