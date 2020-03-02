5834baebf6c8aa99dbd1b92bf4cf39b6c6ea0688
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-02 00:41:24 -0800

    filter products

---
+++ 08-filter.js
@@ -0,0 +1,18 @@
+/* 
+use 'filter' if you want to get a subset of an array
+
+Think of books on amazon.com and you want to select only books
+from a particular author.
+*/
+
+var products = [
+    {name: 'cucumber', type: 'vegetable', qty: 0, price: 1},
+    {name: 'banana', type: 'fruit', qty: 10, price: 15},
+    {name: 'celery', type: 'vegetable', qty: 30, price: 13},
+    {name: 'orange', type: 'fruit', qty: 3, price: 5}
+];
+
+// get subset: vegetables, qty > 0, price < 10
+
+
+
