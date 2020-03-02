b46f183e9f0c2b0d5bd674faab5c29f2c1c245da
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-02 00:17:36 -0800

    filter array-helper

---
+++ 07-filter.js
@@ -0,0 +1,27 @@
+/* 
+use 'filter' if you want to get a subset of an array
+
+Think of books on amazon.com and you want to select only books
+from a particular author.
+*/
+
+var sports = [
+    {name: 'baseball', surface: 'field'},
+    {name: 'basketball', surface: 'court'},
+    {name: 'tennis', surface: 'court'},
+    {name: 'swimming', surface: 'pool'},
+    {name: 'soccer', surface: 'field'}
+];
+
+var filteredSports = [];
+
+/*
+// get only sports played on a field using traditional for-loop
+for (i = 0; i < sports.length; i++) {
+    if (sports[i].surface === 'field') {
+        filteredSports.push(sports[i]);
+    }
+}
+
+console.log(filteredSports);
+*/
