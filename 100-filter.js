06dc17538d7694b353990cb5c46fe64b23e36df7
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2020-03-09 16:57:16 -0700

    refactor

--- 100-filter.js
+++ 100-filter.js
@@ -5,28 +5,29 @@ This is a hard one! Create a function called 'reject'. Reject should work in the
 function returns 'true', the item should *not* be included in the new array. Hint: You can reuse filter.
 */
 
+// SOLUTION ONE:
+
 // define 'reject'
 function reject(array, callback) {
     return array.filter(number => !callback(number));
 }
 
-var numbers = [1, 3, 5, 10, 20, 30]; // array to be passed
+// need an example array to be passed
+var numbers = [1, 3, 5, 10, 20, 30];
 
 // calling 'reject' passing in numbers & callback
 var lessThanFifteen = reject(numbers, function(number){
 	return number > 15;
 });
 
+// print output
 console.log(lessThanFifteen);
 
 
-
+// SOLUTION 2 (must comment out solution 1 definition):
 
 // function reject(array, iteratorFunction) {
 //  return array.filter((number) => {
-//      return !iteratorFunction(number)
+//      return !iteratorFunction(number);
 //  });
 // }
-
-// array = [10, 20, 30, 40, 50]
-// console.log(reject(array, ));
